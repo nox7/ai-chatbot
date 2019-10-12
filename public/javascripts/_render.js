7b539d8b262cfeea50767438c2baab8eff657bd7
nox7 <garetgreen1996@gmail.com>
2019-10-12 10:42:13 -0500

    Add fog and stars

--- public/javascripts/_render.js
+++ public/javascripts/_render.js
@@ -13,16 +13,19 @@ class Renderer{
 
 		this.scene = new THREE.Scene();
 		this.scene.background = new THREE.Color( 0x200e2b );
+		this.scene.fog = new THREE.Fog(0x200e2b, 2000, 2500);
 
-		this.camera = new THREE.PerspectiveCamera( 35, this.container.clientWidth / this.container.clientHeight, 0.1, 2500 );
+		this.camera = new THREE.PerspectiveCamera( 35, this.container.clientWidth / this.container.clientHeight, 0.1, 3500 );
 		this.camera.position.set(-5, 5, 7);
 
 		this.controls = new THREE.OrbitControls(this.camera, this.container);
 
-		this.ambientLight = new THREE.HemisphereLight( 0xddeeff, 0x0f0e0d, 5 );
-		this.directionalLight = new THREE.DirectionalLight(0xffffff, 5);
-		this.directionalLight.position.set(10,10,10);
-		this.scene.add(this.ambientLight, this.directionalLight);
+		this.ambientLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 7 );
+		//this.ambientLight = new THREE.HemisphereLight( 0xddeeff, 0x0f0e0d, 5 );
+		//this.directionalLight = new THREE.DirectionalLight(0xffffff, 5);
+		//this.directionalLight.position.set(10,10,10);
+		//this.scene.add(this.ambientLight, this.directionalLight);
+		this.scene.add(this.ambientLight);
 
 		this.renderer = new THREE.WebGLRenderer();
 		this.renderer.setSize( this.container.clientWidth, this.container.clientHeight );
@@ -34,6 +37,9 @@ class Renderer{
 		this.container.appendChild(this.renderer.domElement);
 
 		this.createMaterialsAndGeometries();
+
+		this.createGridLines();
+		this.createStarPoints();
 
 		window.addEventListener("resize", () => {
 			this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
@@ -104,6 +110,63 @@ class Renderer{
 		}
 
 		NeuronGroup.add(Neuron);
+	}
+
+	getRandomNumber(min, max){
+		return Math.random() * (max-min) + min;
+	}
+
+	/**
+	* Creates star points in space
+	*/
+	createStarPoints(){
+		const dotGeometry = new THREE.Geometry();
+		const dotMaterial = new THREE.PointsMaterial({size:1, sizeAttenuation:false});
+
+		for(let i = 0; i < 1900; i++){
+			dotGeometry.vertices.push(new THREE.Vector3(this.getRandomNumber(-1000, 1000),  this.getRandomNumber(-700, 700), this.getRandomNumber(-1000, 1000)));
+		}
+
+		const dots = new THREE.Points(dotGeometry, dotMaterial);
+		this.scene.add(dots);
+
+
+	}
+
+	/**
+	* Creates the grid lines
+	*
+	*/
+	createGridLines(){
+		// Every 10 units apart
+		const lineGroup = new THREE.Group();
+		this.gridLineMaterial = new THREE.LineBasicMaterial({
+			color: 0xffffff
+		});
+
+		// X axis lines
+		for (let x = -5000; x < 5000; x += 50){
+			let lineGeometry = new THREE.Geometry();
+			lineGeometry.vertices.push(
+				new THREE.Vector3(x, -500, -5000),
+				new THREE.Vector3(x, -500, 5000),
+			)
+			const Line = new THREE.Line(lineGeometry, this.gridLineMaterial);
+			lineGroup.add(Line);
+		}
+
+		// z axis lines
+		for (let z = -5000; z < 5000; z += 50){
+			let lineGeometry = new THREE.Geometry();
+			lineGeometry.vertices.push(
+				new THREE.Vector3(-5000, -500, z),
+				new THREE.Vector3(5000, -500, z),
+			)
+			const Line = new THREE.Line(lineGeometry, this.gridLineMaterial);
+			lineGroup.add(Line);
+		}
+
+		this.scene.add(lineGroup);
 	}
 
 	render(){
