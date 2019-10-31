63eb51ed715ca82790facae849411d4f38cb0832
nox7 <garetgreen1996@gmail.com>
2019-10-31 11:34:04 -0500

    Revert "Use WASD to pan instead of arrows"
    This reverts commit fe73435cb690ef1c40948bc28efa9b41c574df06.

--- public/javascripts/three/orbit-controls.js
+++ public/javascripts/three/orbit-controls.js
@@ -73,7 +73,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 	this.enableKeys = true;
 
 	// The four arrow keys
-	this.keys = { LEFT: 37, UP: 38, RIGHT: 39, BOTTOM: 40, W:87, S:83, A:65, D:68 };
+	this.keys = { LEFT: 37, UP: 38, RIGHT: 39, BOTTOM: 40 };
 
 	// Mouse buttons
 	this.mouseButtons = { LEFT: THREE.MOUSE.ROTATE, MIDDLE: THREE.MOUSE.DOLLY, RIGHT: THREE.MOUSE.PAN };
@@ -558,22 +558,22 @@ THREE.OrbitControls = function ( object, domElement ) {
 
 		switch ( event.keyCode ) {
 
-			case scope.keys.W:
+			case scope.keys.UP:
 				pan( 0, scope.keyPanSpeed );
 				needsUpdate = true;
 				break;
 
-			case scope.keys.S:
+			case scope.keys.BOTTOM:
 				pan( 0, - scope.keyPanSpeed );
 				needsUpdate = true;
 				break;
 
-			case scope.keys.A:
+			case scope.keys.LEFT:
 				pan( scope.keyPanSpeed, 0 );
 				needsUpdate = true;
 				break;
 
-			case scope.keys.D:
+			case scope.keys.RIGHT:
 				pan( - scope.keyPanSpeed, 0 );
 				needsUpdate = true;
 				break;
