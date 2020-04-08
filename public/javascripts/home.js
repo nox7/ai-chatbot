e3d62aa982e3dff4ed9a9eb928a542b4edfceeb1
nox7 <17110935+nox7@users.noreply.github.com>
2020-04-08 14:26:19 -0500

    Add chat logging and server message sending

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,7 +1,26 @@
 import SocketClient from "./_socket-client.js";
+import ChatLog from "./_chat-log.js";
 
-(function(){
+(() => {
 	const socket = new SocketClient();
+	const chatLog = new ChatLog(document.querySelector("#log-container"));
+
+	document.querySelector("#send-form").addEventListener("submit", e => {
+		e.preventDefault();
+
+		let input = document.querySelector("#input-message");
+		const message = input.value.trim();
+
+		if (message.length > 0){
+			input.value = "";
+			chatLog.addMessage(message);
+
+			socket.send(JSON.stringify({
+				event:"message",
+				message:message
+			}));
+		}
+	});
 
 	socket.onError = (err) => {
 		console.log("Error");
